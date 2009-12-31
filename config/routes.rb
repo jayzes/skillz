223d2d1d3d28d8bff6df5d2a5e0f7e70840dc997
Jay Zeschin <jay.zeschin@factorylabs.com>
2009-12-30 23:33:34 -0700

    Added root route to point to /projects and removed default routes

--- config/routes.rb
+++ config/routes.rb
@@ -34,18 +34,18 @@ ActionController::Routing::Routes.draw do |map|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
   
   map.login   '/login', :controller => 'sessions', :action => 'login'
   map.logout  '/logout', :controller => 'sessions', :action => 'logout'
+  
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+  map.root :controller => 'projects'
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
 end
